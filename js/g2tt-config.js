f12f78ebba18692a521e4e1f3316725ed419dc36
G2TTRSS Mobile <g2ttrss@gmail.com>
2013-05-14 20:27:09 -0700

    Moved 'configuration' into a seperate file

---
+++ js/g2tt-config.js
@@ -0,0 +1,9 @@
+// Config
+global_ttrssUrl = "/tt-rss/";
+
+// Preferences
+pref_Feed = "-4"; // Default: all items
+pref_IsCat = false;
+pref_ViewMode = 'unread'; // Default: unread articles only
+pref_TextType = "content"; // Default: full articles
+pref_OrderBy = 'date_reverse'; // Default: oldest first
